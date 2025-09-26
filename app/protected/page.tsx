5a0438b95b6b6aa77ea9f4deac57c84c4e31b3bc
Hadi Khir <khir.hadi@gmail.com>
2025-09-26 18:25:25 -0400

    feat: adding auth

---
+++ app/protected/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from 'next/navigation'
+
+import { LogoutButton } from '@/components/logout-button'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function ProtectedPage() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getClaims()
+  if (error || !data?.claims) {
+    redirect('/auth/login')
+  }
+  const { data: schedules } = await supabase.from('schedule').select();
+
+  return (
+    <div className="flex h-svh w-full items-center justify-center gap-2">
+      <p>
+        Hello <span>{data.claims.email}</span>
+      </p>
+      <pre>{JSON.stringify(schedules, null, 2)}</pre>
+      <LogoutButton />
+    </div>
+  )
+}
